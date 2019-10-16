daa8d78da149247fe3044d84f569ecb5d220f8a0
pollmix <polboy777@gmail.com>
2019-10-17 00:36:38 +0600

    readme added

--- popup.js
+++ popup.js
@@ -1,23 +1,23 @@
 "use strict";
 
 function sayHello() {
-  const button = document.createElement("button");
-  const buttonText = document.createTextNode("Open in CodeSandbox");
+  const btn = document.createElement("button");
+  const btnText = document.createTextNode("Open in CodeSandbox");
 
-  button.appendChild(buttonText);
+  btn.appendChild(btnText);
 
-  button.style.fontSize = "15px";
-  button.style.padding = "0.7em 1.7em";
-  button.style.margin = "0 0.3em 0.3em 0";
-  button.style.borderRadius = "0.2em";
-  button.style.fontWeight = "bold";
-  button.style.color = "#FFFFFF";
-  button.style.backgroundColor = "#3369ff";
-  button.style.border = "0";
-  button.style.textAlign = "center";
-  button.style.cursor = "pointer";
+  btn.style.fontSize = "15px";
+  btn.style.padding = "0.7em 1.7em";
+  btn.style.margin = "0 0.3em 0.3em 0";
+  btn.style.borderRadius = "0.2em";
+  btn.style.fontWeight = "bold";
+  btn.style.color = "#FFFFFF";
+  btn.style.backgroundColor = "#3369ff";
+  btn.style.border = "0";
+  btn.style.textAlign = "center";
+  btn.style.cursor = "pointer";
 
-  button.onclick = function() {
+  btn.onclick = function() {
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
       const url = tabs[0].url;
       const codesandboxUrl = url.replace(
@@ -31,7 +31,7 @@ function sayHello() {
     });
   };
 
-  document.body.appendChild(button);
+  document.body.appendChild(btn);
 }
 
 window.onload = sayHello;
---
+++ readme.md
@@ -0,0 +1,18 @@
+## Open GitHub Repository in CodeSandbox - Chrome Extension
+
+### Installation:
+
+1. Clone or download this repository on your PC.
+2. Open the Extension Management page by navigating to `chrome://extensions`. The Extension Management page can also be opened by clicking on the Chrome menu, hovering over `More Tools` then selecting `Extensions`.
+3. Enable `Developer Mode` by clicking the toggle switch next to Developer mode.
+4. Click the `Load Unpacked` button and select the downloaded extension directory.
+
+![Screenshot](https://cdn1.imggmi.com/uploads/2019/10/16/a27fc6bb73eca00c3187e618bee04aaa-full.png)
+
+### Usage:
+
+If you are on a GitHub repository than press the plugin icon and then press on `Open in CodeSandbox` button in popup.
+
+![Screenshot](https://cdn1.imggmi.com/uploads/2019/10/16/f5f603b4ff793a2628d29bbbf0b043e7-full.png)
+
+**Note:** This plugin only work on `github.com`.
